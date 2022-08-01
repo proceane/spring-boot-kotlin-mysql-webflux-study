2de6be508503b96f7bd0f38d9deb64a7e502c479
proceane <proceane1@gmail.com>
2022-08-02 07:41:42 +0900

    Add author dto inner class

--- src/main/kotlin/com/study/webflux/presentation/dto/AuthorDto.kt
+++ src/main/kotlin/com/study/webflux/presentation/dto/AuthorDto.kt
@@ -1,4 +1,33 @@
 package com.study.webflux.presentation.dto
 
+import com.study.webflux.domain.author.Author
+import java.time.LocalDate
+import java.time.LocalDateTime
+
 class AuthorDto {
+
+    class Request {
+        data class Get(
+            var id: Int
+        ) {
+        }
+    }
+
+    class Response {
+        data class Get(
+            val id: Int?,
+            var name: String,
+            var email: String,
+            var birthdate: LocalDate,
+            var createdAt: LocalDateTime
+        ) {
+            fun of(author: Author): Get = Get(
+                author.id,
+                author.firstName + " " + author.lastName,
+                author.email,
+                author.birthdate,
+                author.createdAt
+            )
+        }
+    }
 }
