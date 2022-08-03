e233c1306f03e9015c3746ab8cb80cf31ed6a9d0
proceane <proceane1@gmail.com>
2022-08-04 07:42:02 +0900

    Implement Author api

--- src/main/kotlin/com/study/webflux/application/service/AuthorService.kt
+++ src/main/kotlin/com/study/webflux/application/service/AuthorService.kt
@@ -1,8 +1,14 @@
 package com.study.webflux.application.service
 
 import com.study.webflux.infra.repository.AuthorRepository
+import com.study.webflux.presentation.dto.AuthorDto
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 
 @Service
 class AuthorService(val authorRepository: AuthorRepository) {
+
+    fun get(id: Int): Mono<AuthorDto.Response.Get> = authorRepository.findById(id)
+        .map { AuthorDto.Response.Get.of(it) }
+
 }
--- src/main/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1.kt
+++ src/main/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1.kt
@@ -1,8 +1,15 @@
 package com.study.webflux.presentation.controller
 
 import com.study.webflux.application.service.AuthorService
+import com.study.webflux.presentation.dto.AuthorDto
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
 class AuthorControllerV1(val authorService: AuthorService) {
+
+    @GetMapping("/author/v1/{id}")
+    fun get(@PathVariable id: Int) : Mono<AuthorDto.Response.Get> = authorService.get(id)
 }
