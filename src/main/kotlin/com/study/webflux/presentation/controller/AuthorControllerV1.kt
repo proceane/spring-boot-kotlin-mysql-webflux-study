492b6533de6d9d717d6d745bd06d89f68f568844
proceane <proceane1@gmail.com>
2022-08-13 07:51:40 +0900

    Implement GET list Author

--- src/main/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1.kt
+++ src/main/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1.kt
@@ -3,6 +3,7 @@ package com.study.webflux.presentation.controller
 import com.study.webflux.application.service.AuthorService
 import com.study.webflux.presentation.dto.AuthorDto
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
@@ -10,6 +11,9 @@ class AuthorControllerV1(val authorService: AuthorService) {
 
     @GetMapping("/author/v1/{id}")
     fun get(@PathVariable id: Int): Mono<AuthorDto.Response.Get> = authorService.get(id)
+
+    @GetMapping("/author/v1")
+    fun getAll(): Flux<AuthorDto.Response.Get> = authorService.getAll()
 
     @PostMapping("/author/v1")
     fun post(@RequestBody dto: AuthorDto.Request.Post): Mono<AuthorDto.Response.Get> = authorService.post(dto)
