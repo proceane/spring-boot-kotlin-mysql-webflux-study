ffd491dc1d8ddf9995dce9c05193626989d0c5d4
proceane <proceane1@gmail.com>
2022-08-15 09:42:08 +0900

    Change author rest api uri

--- src/main/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1.kt
+++ src/main/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1.kt
@@ -7,22 +7,23 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
+@RequestMapping("/v1/authors")
 class AuthorControllerV1(val authorService: AuthorService) {
 
-    @GetMapping("/author/v1/{id}")
+    @GetMapping("/{id}")
     fun get(@PathVariable id: Int): Mono<AuthorDto.Response.Get> = authorService.get(id)
 
-    @GetMapping("/author/v1")
+    @GetMapping
     fun getAll(): Flux<AuthorDto.Response.Get> = authorService.getAll()
 
-    @PostMapping("/author/v1")
+    @PostMapping
     fun post(@RequestBody dto: AuthorDto.Request.Post): Mono<AuthorDto.Response.Get> = authorService.post(dto)
 
-    @PatchMapping("/author/v1/{id}")
+    @PatchMapping("/{id}")
     fun patch(@PathVariable id: Int, @RequestBody dto: AuthorDto.Request.Patch): Mono<AuthorDto.Response.Get> =
         authorService.patch(id, dto)
 
-    @DeleteMapping("/author/v1/{id}")
+    @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Int) = authorService.delete(id)
 
 }
