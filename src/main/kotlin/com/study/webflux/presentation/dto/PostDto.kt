a466867721680febbaca8fe2e968034c4ce0c91d
proceane <proceane1@gmail.com>
2022-08-16 09:19:34 +0900

    Add classes in PostDto.kt

--- src/main/kotlin/com/study/webflux/presentation/dto/PostDto.kt
+++ src/main/kotlin/com/study/webflux/presentation/dto/PostDto.kt
@@ -1,4 +1,31 @@
 package com.study.webflux.presentation.dto
 
+import java.time.LocalDateTime
+
 class PostDto {
+
+    class Request {
+        data class Post(
+            var title: String,
+            var description: String,
+            var content: String,
+        )
+
+        data class Patch(
+            var title: String?,
+            var description: String?,
+            var content: String?,
+        )
+    }
+
+    class Response {
+        data class Get(
+            val id: Int?,
+            var authorId: Int,
+            var title: String,
+            var description: String,
+            var content: String,
+            var createdAt: LocalDateTime
+        )
+    }
 }
