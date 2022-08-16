29592cad6e672690cfce514871af2ce1af907d26
proceane <proceane1@gmail.com>
2022-08-17 07:30:43 +0900

    Implement Get by post id

--- src/main/kotlin/com/study/webflux/application/service/PostService.kt
+++ src/main/kotlin/com/study/webflux/application/service/PostService.kt
@@ -1,8 +1,13 @@
 package com.study.webflux.application.service
 
 import com.study.webflux.infra.repository.PostRepository
+import com.study.webflux.presentation.dto.PostDto
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 
 @Service
 class PostService(val postRepository: PostRepository) {
+
+    fun get(id: Int): Mono<PostDto.Response.Get> = postRepository.findById(id)
+        .map { PostDto.Response.Get.of(it) }
 }
