dbd8ad632fe9efe881ba6581b4dedbdc81add721
proceane <proceane1@gmail.com>
2022-09-08 07:44:17 +0900

    Add delete test

--- src/test/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1Test.kt
+++ src/test/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1Test.kt
@@ -130,7 +130,11 @@ class AuthorControllerV1Test() {
 
     @Test
     fun delete() {
-
+        webTestClient.patch().uri("/v1/authors/1").accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectBody()
     }
 
 }
