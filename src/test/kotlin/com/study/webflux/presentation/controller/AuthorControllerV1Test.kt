0a683ceb3c317ef8c1b1b5c78ea85cffc3890e16
proceane <proceane1@gmail.com>
2022-09-08 19:14:11 +0900

    Change patch to delete

--- src/test/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1Test.kt
+++ src/test/kotlin/com/study/webflux/presentation/controller/AuthorControllerV1Test.kt
@@ -130,7 +130,7 @@ class AuthorControllerV1Test() {
 
     @Test
     fun delete() {
-        webTestClient.patch().uri("/v1/authors/1").accept(MediaType.APPLICATION_JSON)
+        webTestClient.delete().uri("/v1/authors/1").accept(MediaType.APPLICATION_JSON)
             .exchange()
             .expectStatus()
             .isOk()
