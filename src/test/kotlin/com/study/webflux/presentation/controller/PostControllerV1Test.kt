08932dde7591b4fa13d1fecc91ca7d9ef39ce1f8
proceane <proceane1@gmail.com>
2022-09-20 21:18:56 +0900

    Ready post controller test

--- src/test/kotlin/com/study/webflux/presentation/controller/PostControllerV1Test.kt
+++ src/test/kotlin/com/study/webflux/presentation/controller/PostControllerV1Test.kt
@@ -1,7 +1,39 @@
 package com.study.webflux.presentation.controller
 
+import com.study.webflux.infra.repository.PostRepository
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.restdocs.RestDocumentationExtension
+import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation
+import org.springframework.test.context.TestConstructor
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient
+import org.springframework.web.context.WebApplicationContext
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@ExtendWith(RestDocumentationExtension::class)
 class PostControllerV1Test() {
+
+    lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    lateinit var postRepository: PostRepository
+
+    @BeforeEach
+    fun setUp(context: WebApplicationContext, restDocumentation: RestDocumentationContextProvider) {
+        this.webTestClient = MockMvcWebTestClient.bindToApplicationContext(context)
+            .configureClient()
+            .filter(WebTestClientRestDocumentation.documentationConfiguration(restDocumentation))
+            .build()
+    }
 
 }
