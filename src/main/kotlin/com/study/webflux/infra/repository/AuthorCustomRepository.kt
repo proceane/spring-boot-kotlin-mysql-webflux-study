33b354bcdf1c0b51453c2bb0ff06eca1ebdade79
proceane <proceane1@gmail.com>
2022-10-13 20:56:17 +0900

    Add findAll in AuthorCustomRepository

--- src/main/kotlin/com/study/webflux/infra/repository/AuthorCustomRepository.kt
+++ src/main/kotlin/com/study/webflux/infra/repository/AuthorCustomRepository.kt
@@ -1,4 +1,10 @@
 package com.study.webflux.infra.repository
 
+import com.study.webflux.presentation.dto.AuthorDto
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import reactor.core.publisher.Mono
+
 interface AuthorCustomRepository {
+    fun findAll(pageable: Pageable): Mono<Page<AuthorDto.Response.Get>>
 }
