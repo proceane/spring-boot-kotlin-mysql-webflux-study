59a20a015f6b6b2a899ed96ebbbf2b1fc6321463
proceane <proceane1@gmail.com>
2022-10-14 21:03:28 +0900

    Change Page<AuthorDto.Response.Get> to Page<Author> in AuthorCustomRepository

--- src/main/kotlin/com/study/webflux/infra/repository/AuthorCustomRepository.kt
+++ src/main/kotlin/com/study/webflux/infra/repository/AuthorCustomRepository.kt
@@ -1,10 +1,10 @@
 package com.study.webflux.infra.repository
 
-import com.study.webflux.presentation.dto.AuthorDto
+import com.study.webflux.domain.author.Author
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import reactor.core.publisher.Mono
 
 interface AuthorCustomRepository {
-    fun findAll(pageable: Pageable): Mono<Page<AuthorDto.Response.Get>>
+    fun findAll(pageable: Pageable): Mono<Page<Author>>
 }
