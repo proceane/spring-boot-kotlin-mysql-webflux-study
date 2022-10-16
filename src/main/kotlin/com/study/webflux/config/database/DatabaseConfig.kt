de0d3ca305f50be900211ac4124c804706153e1e
proceane <proceane1@gmail.com>
2022-10-16 10:23:22 +0900

    Create DatabaseConfig

---
+++ src/main/kotlin/com/study/webflux/config/database/DatabaseConfig.kt
@@ -0,0 +1,10 @@
+package com.study.webflux.config.database
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.transaction.annotation.EnableTransactionManagement
+
+@Configuration
+@EnableR2dbcRepositories
+@EnableTransactionManagement
+class DatabaseConfig
